bb70d8c4aa7570d7b2a0d83e83e12925a97e4045
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-25 20:31:08 +0900

    Project Init

---
+++ src/main/java/com/toggn/mma/MilitaryManpowerAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.toggn.mma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilitaryManpowerAdministrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MilitaryManpowerAdministrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toggn/mma/MilitaryManpowerAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toggn.mma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilitaryManpowerAdministrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
