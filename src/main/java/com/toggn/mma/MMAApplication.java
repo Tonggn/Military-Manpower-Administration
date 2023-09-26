1ba4781c460ea7c75a8ad054586f8abfd6f8da5a
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-26 16:14:35 +0900

    refactor: Application 클래스명 수정

---
+++ src/main/java/com/toggn/mma/MMAApplication.java
@@ -0,0 +1,13 @@
+package com.toggn.mma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MMAApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(MMAApplication.class, args);
+    }
+
+}
