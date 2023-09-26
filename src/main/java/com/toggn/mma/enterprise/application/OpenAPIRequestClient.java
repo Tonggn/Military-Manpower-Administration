b76fb2f908b7f12be5f11c7bda361272762e8bca
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-26 19:27:46 +0900

    refactor: 공공 api 요청 클라이언트 빈 등록 및 패키지, 클래스명 수정

---
+++ src/main/java/com/toggn/mma/enterprise/application/OpenAPIRequestClient.java
@@ -0,0 +1,33 @@
+package com.toggn.mma.enterprise.application;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class OpenAPIRequestClient {
+
+    private static final String ALL_ROWS_SIZE = "99999";
+
+    @Value("${mma.itp.enterprise.client.requestUrl}")
+    private String requestUrl;
+    @Value("${mma.itp.enterprise.client.secretKey}")
+    private String secretKey;
+
+    public Document requestAllEnterprises() {
+        final String url = requestUrl + "?ServiceKey=" + secretKey + "&numOfRows=" + ALL_ROWS_SIZE;
+
+        final Connection connect = Jsoup.connect(url).maxBodySize(0);
+
+        try {
+            return Jsoup.parse(connect.get().html(), "", Parser.xmlParser());
+        } catch (final IOException e) {
+            throw new IllegalArgumentException("Invalid url: " + url, e);
+        }
+    }
+}
