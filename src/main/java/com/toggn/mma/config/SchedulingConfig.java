4c530588dcb53a4e7c842701f335a9839768327a
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-26 22:27:09 +0900

    feat: 스케줄링 설정 활성화

---
+++ src/main/java/com/toggn/mma/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package com.toggn.mma.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
