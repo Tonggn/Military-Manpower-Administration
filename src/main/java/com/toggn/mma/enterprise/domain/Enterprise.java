1c639284c8e2fc26220323d6ca7a6412c874349d
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-26 16:45:42 +0900

    refactor: Enterprise의 name에 unique 제약조건 추가

--- src/main/java/com/toggn/mma/enterprise/domain/Enterprise.java
+++ src/main/java/com/toggn/mma/enterprise/domain/Enterprise.java
@@ -14,7 +14,7 @@ public class Enterprise {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     @Enumerated(EnumType.STRING)
